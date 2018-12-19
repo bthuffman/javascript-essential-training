bcb8558b8ad0932a0ab8b1e14d9253a75c269906
Brandon Huffman <bt_huffman@msn.com>
2018-12-19 12:59:51 -0700

    Completed chapter 4

--- Ex_Files_JavaScript_EssT/Exercise Files/04_12/script.js
+++ Ex_Files_JavaScript_EssT/Exercise Files/04_12/script.js
@@ -47,7 +47,25 @@ console.log("The result: ", theResult());
 
 	return multiply;
 //The function multiple relies on outside variables defined outside of it, therefore the computer keeps the variables a and b alive instead of disposing of them.
-//Eseentially a closure is.. A function inside a function that relies on the outside variables of the other function to work.
+//Eseentially a closure is.. A function inside a function that relies on the variables of the outside function to work.
 
 *************************************************************/
 //Practical applications....
+function giveMeEms(pixels) {
+	var baseValue = 16;
+
+	function doTheMath() {
+		return pixels/baseValue;
+	}
+	return doTheMath;
+}
+
+var smallSize = giveMeEms(12);
+var mediumSize = giveMeEms(18);
+var largeSize = giveMeEms(24);
+var xlargeSize = giveMeEms(32);
+
+console.log("Small size: ", smallSize());
+console.log("Medium size: ", mediumSize());
+console.log("Large size: ", largeSize());
+console.log("Extra large size: ", xlargeSize());
